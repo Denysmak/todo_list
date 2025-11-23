8928d349058c3cd77d651154cfa5760d5be6b150
denys <denysmakene13@gmail.com>
2025-11-22 21:21:27 -0300

    Configura basePath /tasks

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Define que o app roda dentro deste caminho
+  basePath: '/tasks',
+  
+  // Opcional: Força a barra no final (ex: /tasks/dashboard/)
+  // Ajuda em alguns casos de SEO e roteamento estrito
+  trailingSlash: true, 
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  
-  // Application is served under a sub-path in production (e.g. https://meu-dominio.com/tasks)
-
 };
 
 module.exports = nextConfig;
